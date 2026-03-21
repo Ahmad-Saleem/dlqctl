e008ee3f9b729c8d28dfe0eaf0713916a4a8e0fd
Ahmad <it_saleem@hotmail.com>
2026-03-21 21:43:00 +0000

    feat: Add persistent 'region' flag

--- cmd/root.go
+++ cmd/root.go
@@ -10,3 +10,7 @@ var rootCmd = &cobra.Command{
 func Execute() error {
 	return rootCmd.Execute()
 }
+
+func init() {
+	rootCmd.PersistentFlags().StringP("region", "R", "eu-west-1", "AWS Region")
+}
